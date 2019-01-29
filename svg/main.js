10eb63d150cf5324319f9755ea34af854da6176a
Munkhtegsh Munkhbat <munkhtegsh.m@gmail.com>
2019-01-28 22:44:54 -0800

    Setup main data

--- svg/main.js
+++ svg/main.js
@@ -1,95 +1,57 @@
+// Setup margin
+let margin = {top: 10, right: 20, bottom: 30, left: 40};
+let width = 600 - margin.left - margin.right;
+let height = 400 - margin.top - margin.bottom;
+
+// positioning graph in the svg
+let g = d3
+  .select('#chart-area')
+  .append('svg')
+  .attr('width', width)
+  .attr('height', height)
+  .append('g')
+  .attr('transform', 'translate(' + margin.left + ', ' + margin.top + ')');
+
+// fetching data
 d3.json('./data.json').then(res => {
-  let data = res.map(item => {
-    return {name: item.name, height: +item.height};
+  res.forEach(item => {
+    item.revenue = +item.revenue;
   });
 
-  let margin = {top: 20, right: 10, bottom: 20, left: 100};
-  let width = 960 - margin.left - margin.right;
-  let height = 500 - margin.top - margin.bottom;
+  // Scale y-axis
+  let y = d3
+    .scaleLinear() // fn scales graph to fit in height of the svg
+    .domain([0, 54000]) // input
+    .range([0, 300]); // output
 
-  let svg = d3
-    .select('.row')
-    .append('svg')
-    .attr('width', width + margin.left + margin.right)
-    .attr('height', height + margin.top + margin.bottom);
-
-  let g = svg
-    .append('g')
-    .attr('transform', 'translate(' + margin.left + ', ' + margin.top + ')');
-
+  // Scale x-axis
   let x = d3
-    .scaleBand()
-    .domain(data, function(e) {
-      return e.name;
-    })
+    .scaleBand() // fn scales graph to fit in width of the svg
+    .domain(res.map(d => d.revenue))
     .range([0, width])
-    .paddingInner(0, 3)
+    .paddingInner(0.3)
     .paddingOuter(0.3);
 
-  let y = d3
-    .scaleLinear()
-    .domain([
-      0,
-      d3.max(data, function(e) {
-        return e.height;
-      }),
-    ])
-    .range([0, height]);
+  // d3.max(function(d) {
+  //   return d.revenue;
+  // }),
 
-  let squares = g
+  // Select 'rect' && Enter data into svg
+  let rect = g
     .selectAll('rect')
-    .data(data)
+    .data(res)
     .enter();
-
-  squares
+  rect
     .append('rect')
-    .attr('x', (item, i) => {
-      return i * 100;
+    .attr('x', function(d, i) {
+      // x-axys starting point
+      return x(d.month);
     })
-    .attr('y', 0)
-    .attr('height', item => {
-      return y(item.height);
+    .attr('y', function(d, i) {
+      // y-axys starting point
+      return y(d.revenue);
     })
-    .attr('width', item => {
-      return '15px';
-    })
-    .attr('fill', 'orange');
+    .attr('width', 10) // width of each bars
+    .attr('height', d => d.revenue) // height of each bars
+    .attr('fill', 'brown');
 });
-
-// d3.json('./data.json').then(data => {
-//   data.forEach(d => {
-//     console.log(d);
-//     d.age = +d.age;
-//   });
-
-//   let svg = d3
-//     .select('#chart-are')
-//     .append('svg')
-//     .attr('width', 2000)
-//     .attr('height', 300);
-
-//   let rectangles = svg.selectAll('rect').data(data);
-
-//   rectangles
-//     .enter()
-//     .append('rect')
-//     .attr('x', (d, i) => {
-//       return i * 200;
-//     })
-//     .attr('y', (d, i) => {
-//       return 0;
-//     })
-//     .attr('width', d => {
-//       return d.age * 5;
-//     })
-//     .attr('height', d => {
-//       return d.age * 5;
-//     })
-//     .attr('fill', d => {
-//       if (d.age === 10) {
-//         return 'blue';
-//       } else {
-//         return 'green';
-//       }
-//     });
-// });
